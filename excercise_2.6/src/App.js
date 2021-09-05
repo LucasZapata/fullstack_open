5b9d26942f856e67638fa24b85b3407f6486e44c
RoboMambo <56557697+RoboMambo@users.noreply.github.com>
2021-09-05 05:30:12 -0300

    2.19-2.20

--- excercise_2.6/src/App.js
+++ excercise_2.6/src/App.js
@@ -3,6 +3,7 @@ import NetworkComm from './components/NetworkComm.js'
 import PhoneList from './components/PhoneList.js'
 import NewEntry from './components/NewEntry.js'
 import Filter from './components/Filter.js'
+import Notification from './components/Notification.js'
 
 
 
@@ -12,6 +13,12 @@ const App = () => {
   const [newNumber, setNewNumber] = useState('')
   const [filterForm, setFilterForm] = useState('')
   const [filterTerm, setFilterTerm] = useState('')
+  const [notificationMessage, setNotificationMessage] = useState(null)
+  const [warningMessage, setWarningMessage] = useState(null)
+  
+
+  const cssWarning = {color: 'red', background: 'lightgrey', borderStyle: 'solid', borderRadius: 5, padding: 10, fontSize:20}
+  const cssNotification = {color: 'green', background: 'lightgrey', borderStyle: 'solid', borderRadius: 5, padding: 10, fontSize:20}
 
   useEffect(() => {
     const dbData = NetworkComm.GetDB().then(data => setPersons(data))}, [])
@@ -29,23 +36,35 @@ const App = () => {
     const existingData = persons.find(n => n.name === newName)
     console.log(existingData)
     if (existingData === undefined) {
-      const dbData = NetworkComm.AddDB(newPerson).then(data => setPersons(persons.concat(data)))
+      const dbData = NetworkComm.AddDB(newPerson).then(data => {
+        setPersons(persons.concat(data))
+        setNotificationMessage(`${data.name} added to database`)
+        setTimeout(() => {setNotificationMessage(null)}, 5000)
+      }
+        )
     }
     else if (existingData.number !== newPerson.number){
       const confirmation = window.confirm(`Change number for ${newName}?`)
       if (confirmation){
       const dbData = NetworkComm.EditDB(existingData.id, newPerson).then(data => {
-        setPersons(persons.map(p => p.id !== existingData.id ? p: data))}) 
+        setPersons(persons.map(p => p.id !== existingData.id ? p: data))
+        setNotificationMessage(`${data.name} edited`)
+        setTimeout(() => {setNotificationMessage(null)}, 5000)}) 
       }
     }
     else
-      {window.alert(`${newName}, number ${newNumber}, is already added to phonebook.`)}
+      {window.alert(`${newName}, number ${newNumber}, is already added to phonebook.`)
+      setNotificationMessage(`${newName} already present`)
+      setTimeout(() => {setNotificationMessage(null)}, 5000)}
   }
 
   const deletePerson = (personId, personName) => {
     const confirmation = window.confirm(`Delete entry ${personName}?`)
     if (confirmation){
-      const dbData = NetworkComm.RemoveDB(personId).then(setPersons(persons.filter(p => p.id !== personId)))     
+      const dbData = NetworkComm.RemoveDB(personId).
+        then(setPersons(persons.filter(p => p.id !== personId))).
+          catch(error => {setWarningMessage(`Information of ${personName} not present in server`)
+          setTimeout(() => {setWarningMessage(null)}, 5000)})
     }
   }
 
@@ -53,6 +72,8 @@ const App = () => {
     <div>
       <h2>Phonebook</h2>
       <Filter FilterTerm={filterForm} onFilterChange={writeFilterForm} SetFilter={setFilter} />
+      <Notification message = {warningMessage} style = {cssWarning}/>
+      <Notification message = {notificationMessage} style = {cssNotification}/>
       <NewEntry newName={newName} newNumber={newNumber} onTextChange={writeName} onNumberChange={writeNumber} onSubmit={addName} />
       <h2>Numbers</h2>
       <PhoneList entries={persons} filter={filterTerm} deleteEntry={deletePerson} />
